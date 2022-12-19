ed579cdc09a4f5a322b4fe5e49a33041c3c74109
tylerfurgalack <tylerfurgalack@gmail.com>
2022-12-19 15:56:41 -0500

    About component

---
+++ src/components/Project.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Project.css';
+
+function Project() {
+  return <div className="project">{/* Add your project content here */}</div>;
+}
+
+export default Project;
---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Projects.css';
+import Project from './Project';
+
+function Projects() {
+  return (
+    <div className="projects">
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+    </div>
+  );
+}
+
+export default Projects;
