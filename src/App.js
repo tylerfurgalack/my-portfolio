de4ee1bb0b8e4ccf8b643bddbae5e954d42d13dd
tylerfurgalack <tylerfurgalack@gmail.com>
2022-12-19 15:57:08 -0500

    project component and projects container component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import Navbar from './components/Navbar';
 import './App.css';
+import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <About />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about">
+      <h2>Tyler Furgalack</h2>
+      <h3>Aspiring professional Web developer</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra
+        justo in nisi placerat, non ullamcorper lacus iaculis. Mauris velit
+        ligula, suscipit eu placerat in, iaculis a nulla. Aliquam bibendum diam
+        ut ultricies elementum. Ut viverra placerat mi non ornare. In hac
+        habitasse platea dictumst. Nam consectetur nisi quis porttitor luctus.
+        Quisque a mauris metus. Maecenas ultricies interdum ex, at pharetra
+        diam.
+      </p>
+    </div>
+  );
+}
+
+export default About;
